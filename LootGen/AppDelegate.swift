e0783c488fcc09f19bae656185189fd083f76783
Harry Culpan <harry@culpan.org>
2017-06-23 14:33:34 -0400

    Initial Commit

---
+++ LootGen/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LootGen
+//
+//  Created by Harry Culpan on 6/23/17.
+//  Copyright © 2017 Harry Culpan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LootGen/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LootGen
+//
+//  Created by Harry Culpan on 6/23/17.
+//  Copyright © 2017 Harry Culpan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
